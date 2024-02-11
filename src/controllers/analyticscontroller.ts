2a7f1d8625876a5a3297ede53239780449b13206
onody <onoderayuto@onody00.local>
2024-02-11 13:59:32 +0900

    redisのkeyにuuidを追加した

--- src/controllers/analyticscontroller.ts
+++ src/controllers/analyticscontroller.ts
@@ -10,6 +10,8 @@ const redisPort = 6379;
 // const redis = new Redis(redisPort, "127.0.0.1"); //production
 const redis = new Redis(redisPort, "redis"); //local
 
+const { v4: uuidv4 } = require('uuid');
+const uniqueId = uuidv4();
 export const handleAnalyticsPost = (req: Request, res: Response) => {
   try {
     // リクエストデータを受け取る
@@ -19,12 +21,15 @@ export const handleAnalyticsPost = (req: Request, res: Response) => {
     const dateData = displayRequest.getTimestamp();
     console.log('パス名'+requestBody.path)
     console.log("ホスト名"+requestBody.hostname);
+    //if(!new Profile().containsDomain(requestBody.path)){ //local
     if(!new Profile().containsDomain(requestBody.hostname)){
       return false;
     }
     console.log("ホスト名"+requestBody.hostname);
+    //redis.get(requestBody.path, (err, redisResult: any) => { //local
     redis.get(requestBody.hostname, (err, redisResult: any) => {
       if(redisResult !== null && new Date(redisResult) < new Date()|| redisResult === null){
+        //redis.set(requestBody.path,dateData.ymdDate), (err:any) => { //local
         redis.set(requestBody.hostname,dateData.ymdDate), (err:any) => {
           if (err) {
             console.error("Redis エラー:", err);
@@ -33,10 +38,11 @@ export const handleAnalyticsPost = (req: Request, res: Response) => {
           }
         }
       }
-      const postData = `Hash化したIP: ${hashedIp}, テキスト: ${requestBody.text},ボタンテキスト: ${requestBody.buttonText},リンクテキスト: ${requestBody.linkText},
-      リンクURL: ${requestBody.linkUrl},セレクトボックス：${requestBody.selectBoxName},セレクトボックスID：${requestBody.selectBoxNameId},
-      ラジオボタン名：${requestBody.radioButtonName},ラジオボタンラベル名: ${requestBody.labelText},動画再生時間（秒）:${requestBody.videoPlayedTime}`;
-      redis.set(requestBody.hostname+'_'+requestBody.path+'_'+requestBody.action+'_'+hashedIp+'(('+dateData.ymdhmsmDate+'))', postData);
+      //ホスト名、パス名、行動、Hash化したIP,テキスト,ボタンテキスト,リンクテキスト,リンクURL,セレクトボックス,セレクトボックスID,ラジオボタン名,ラジオボタンラベル名,動画再生時間（秒）,年月日,年月日分秒ミリ秒;
+      //CSV出力かつ、DBテーブルに入れるので順番は大事
+      const postData = `${requestBody.hostname},${requestBody.path},${requestBody.action},${hashedIp},${requestBody.text},${requestBody.buttonText}, ${requestBody.linkText},${requestBody.linkUrl},${requestBody.selectBoxName}
+      ,${requestBody.selectBoxNameId},${requestBody.radioButtonName},${requestBody.labelText},${requestBody.videoPlayedTime},${dateData.ymdDate},${dateData.ymdhmsmDate}`;
+      redis.set(requestBody.hostname+'_'+requestBody.path+'_'+requestBody.action+'_'+uniqueId+'(('+dateData.ymdhmsmDate+'))', postData);
     });
 
     console.log(
